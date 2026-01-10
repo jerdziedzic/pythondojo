2670f80e4543088516ba32d2b39821a61af1538a
jerdziedzic <jdziedzic@gmail.com>
2026-01-10 05:58:10 -0600

    Very early structure

--- combat.py
+++ combat.py
@@ -1,2 +1,24 @@
-# Basic combat system here
+"""
+Basic combat system
+"""
+
+import random
+
+# Set combat values for player and enemy
+player_hp = 10
+player_damage = 2
+player_hit = 80
+
+enemy_hp = 10
+enemy_damage = 2
+enemy_hit = 80
+
+# Determine who goes first
+
+# Player takes a swing, if hit, inflict damage and subtract from enemy HP.
+# If enemy HP is equal to or less than 0, go to player victory, otherwise, go to enemy turn.
+
+# Enemy takes a swing, if hit, inflict damage and subtract from player HP.
+# If player HP is equal to or less than 0, go to game over, otherwise, go to player turn.
+
 print("Hello again!")
