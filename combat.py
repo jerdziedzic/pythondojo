37c1933e708e3d85b9ce894ae90ad4b01fd5e422
jerdziedzic <jdziedzic@gmail.com>
2026-01-21 04:06:28 -0600

    Incorporated classes into the main combat.py code

--- combat.py
+++ combat.py
@@ -10,6 +10,20 @@ import random # For random number generation
 import time # For sleep pauses
 
 
+# Class to store stats for player and enemies
+class Character:
+
+    def __init__(self, name, hp, hp_max, damage_min, damage_max, hit, start_quote, win_quote):
+        self.name = name
+        self.hp = hp
+        self.hp_max = hp_max
+        self.damage_min = damage_min
+        self.damage_max = damage_max
+        self.hit = hit
+        self.start_quote = start_quote
+        self.win_quote = win_quote
+
+
 # Function to clear the screen
 def clear_screen():
     # Check the operating system name
@@ -44,40 +58,26 @@ def show_statbar(stat_title, stat_current, stat_max):
     print(f"] ({stat_current}/{stat_max})")
 
 # Set combat values for player and enemy
-player_name = "Hero Joe"
-player_hp = 10
-player_hp_max = 10
-player_damage_min = 1
-player_damage_max = 3
-player_hit = 80
-player_start_quote = "You killed my father. Prepare to die."
-player_win_quote = "You can't stand up to my Mersenne Twister!"
+player=Character("Hero Joe", 10, 10, 1, 3, 80, "You killed my father. Prepare to die.", "You can't stand up to my Mersenne Twister!")
 
-enemy_name = "Punchley"
-enemy_hp = 10
-enemy_hp_max = 10
-enemy_damage_min = 1
-enemy_damage_max = 3
-enemy_hit = 80
-enemy_start_quote = "Get ready to taste pavement, brat."
-enemy_win_quote = "Kid, your dad was much stronger than you are."
+enemy=Character("Punchley", 10, 10, 1, 3, 80, "Get ready to taste pavement, brat.", "Kid, your dad was much stronger than you are.")
 
 # Clear screen, display start quotes and text
 clear_screen()
 
-print(f"{player_name} vs {enemy_name}")
+print(f"{player.name} vs {enemy.name}")
 
-print(f"\n{player_name}: {player_start_quote}")
-print(f"{enemy_name}: {enemy_start_quote}")
+print(f"\n{player.name}: {player.start_quote}")
+print(f"{enemy.name}: {enemy.start_quote}")
 time.sleep(3) # Pause for X seconds
 
 # Determine who goes first and display the result
 # TODO: better initiative system
 turn_order = random.randint(1,2)
 if turn_order == 1:
-    print(f"\n{player_name} gets the first strike!")
+    print(f"\n{player.name} gets the first strike!")
 elif turn_order == 2:
-    print(f"\n{enemy_name} gets the first strike!")
+    print(f"\n{enemy.name} gets the first strike!")
 time.sleep(3) # Pause for X seconds
 
 while True: # Infinite loop until one side loses
@@ -85,27 +85,27 @@ while True: # Infinite loop until one side loses
     # Player takes a swing, if hit, inflict damage and subtract from enemy HP.    
     if turn_order == 1:
         clear_screen()
-        show_statbar(player_name, player_hp, player_hp_max)
-        show_statbar(enemy_name, enemy_hp, enemy_hp_max)
+        show_statbar(player.name, player.hp, player.hp_max)
+        show_statbar(enemy.name, enemy.hp, enemy.hp_max)
         input("\nPress Enter to attack...") # TODO: better player input
-        print(f"\n{player_name} takes a swing!")
+        print(f"\n{player.name} takes a swing!")
         to_hit = random.randint(1, 100)
         print(f"To hit roll: {to_hit}")
-        if to_hit <= player_hit:
-            damage = random.randint(player_damage_min, player_damage_max)
-            print(f"{player_name} hits {enemy_name} for {damage} damage!")
-            enemy_hp -= damage
+        if to_hit <= player.hit:
+            damage = random.randint(player.damage_min, player.damage_max)
+            print(f"{player.name} hits {enemy.name} for {damage} damage!")
+            enemy.hp -= damage
             time.sleep(3)
         else:
-            print(f"{player_name} misses {enemy_name}!")
+            print(f"{player.name} misses {enemy.name}!")
             time.sleep(3)
         # If enemy HP is equal to or less than 0, go to player victory, otherwise, go to enemy turn.
-        if enemy_hp <= 0:
+        if enemy.hp <= 0:
             clear_screen()
-            show_statbar(player_name, player_hp, player_hp_max)
-            show_statbar(enemy_name, enemy_hp, enemy_hp_max)
-            print(f"\n{player_name} wins!")
-            print(f"{player_name}: {player_win_quote}") 
+            show_statbar(player.name, player.hp, player.hp_max)
+            show_statbar(enemy.name, enemy.hp, enemy.hp_max)
+            print(f"\n{player.name} wins!")
+            print(f"{player.name}: {player.win_quote}") 
             time.sleep(3)
             break
         turn_order = 2
@@ -113,31 +113,31 @@ while True: # Infinite loop until one side loses
     # Enemy takes a swing, if hit, inflict damage and subtract from player HP.
     if turn_order == 2:
         clear_screen()
-        show_statbar(player_name, player_hp, player_hp_max)
-        show_statbar(enemy_name, enemy_hp, enemy_hp_max)
-        print(f"\n{enemy_name} takes a swing!")
+        show_statbar(player.name, player.hp, player.hp_max)
+        show_statbar(enemy.name, enemy.hp, enemy.hp_max)
+        print(f"\n{enemy.name} takes a swing!")
         to_hit = random.randint(1, 100)
         print(f"To hit roll: {to_hit}")
-        if to_hit <= enemy_hit:
-            damage = random.randint(enemy_damage_min, enemy_damage_max)
-            print(f"{enemy_name} hits {player_name} for {damage} damage!")
-            player_hp -= damage
+        if to_hit <= enemy.hit:
+            damage = random.randint(enemy.damage_min, enemy.damage_max)
+            print(f"{enemy.name} hits {player.name} for {damage} damage!")
+            player.hp -= damage
             time.sleep(3)
         else:
-            print(f"{enemy_name} misses {player_name}!")
+            print(f"{enemy.name} misses {player.name}!")
             time.sleep(3)
         # If player HP is equal to or less than 0, go to game over, otherwise, go to player turn.
-        if player_hp <= 0:
+        if player.hp <= 0:
             clear_screen()
-            show_statbar(player_name, player_hp, player_hp_max)
-            show_statbar(enemy_name, enemy_hp, enemy_hp_max)
-            print(f"\n{player_name} loses!")
-            print(f"{enemy_name}: {enemy_win_quote}") 
+            show_statbar(player.name, player.hp, player.hp_max)
+            show_statbar(enemy.name, enemy.hp, enemy.hp_max)
+            print(f"\n{player.name} loses!")
+            print(f"{enemy.name}: {enemy.win_quote}") 
             time.sleep(3)
             break
         turn_order = 1
 
 clear_screen()
 print("Combat over! Final results:")
-print(f"{player_name} HP: {player_hp}")
-print(f"{enemy_name} HP: {enemy_hp}")
+print(f"{player.name} HP: {player.hp}")
+print(f"{enemy.name} HP: {enemy.hp}")
